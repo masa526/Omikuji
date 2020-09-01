b0e7dcdcb293a4419501e36de4e2fbed42b289ba
masa526 <masa526sk8@gmail.com>
2020-09-01 22:09:45 +0900

    末吉を追加

--- js/main.js
+++ js/main.js
@@ -5,18 +5,20 @@
 
     
     btn.addEventListener('click', () => {
-        const n = Math.floor(Math.random() * 3);
-        // btn.textContent = n;
-        switch (n) {
-            case 0:
-                btn.textContent = '大吉';
-                break;
-            case 1:
-                btn.textContent = '中吉';
-                break;
-            case 2:
-                btn.textContent = '小吉';
-                break;
-        }
+        const results = ['大吉', '中吉', '凶', '末吉'];
+        const n = Math.floor(Math.random() * results.length);
+        btn.textContent = results[n];
+
+        // switch (n) {
+        //     case 0:
+        //         btn.textContent = '大吉';
+        //         break;
+        //     case 1:
+        //         btn.textContent = '中吉';
+        //         break;
+        //     case 2:
+        //         btn.textContent = '小吉';
+        //         break;
+        // }
     });
 }
