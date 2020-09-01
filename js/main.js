d2b1fcde202e4b6f530e3de6461e2ffa3375b73e
masa526 <masa526sk8@gmail.com>
2020-09-01 22:13:55 +0900

    リファクタ

--- js/main.js
+++ js/main.js
@@ -7,17 +7,5 @@
     btn.addEventListener('click', () => {
         const results = ['大吉', '中吉', '凶', '末吉'];
         btn.textContent = results[Math.floor(Math.random() * results.length)];
-
-        // switch (n) {
-        //     case 0:
-        //         btn.textContent = '大吉';
-        //         break;
-        //     case 1:
-        //         btn.textContent = '中吉';
-        //         break;
-        //     case 2:
-        //         btn.textContent = '小吉';
-        //         break;
-        // }
     });
 }
